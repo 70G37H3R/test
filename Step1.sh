64aac4ae4c97618cc140ecea3dd93884a1a25e8c
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-01 22:02:22 +0700

    Create Step1.sh

---
+++ Step1.sh
@@ -0,0 +1,12 @@
+apt update -y 
+sudo apt install openjdk-8-jdk scala -y
+wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+tar xvf spark-3.0.1-bin-hadoop3.2.tgz
+sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark
+nano ~/.bashrc
+sudo cat >> /etc/docker/daemon.json <<EOF
+export SPARK_HOME=/opt/spark
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
+export PYSPARK_PYTHON=/usr/bin/python3
+EOF
+source ~/.bashrc
