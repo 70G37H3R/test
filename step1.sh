6bdb2cce52b390c64e81ed181e64828305c4e017
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-04 01:08:08 +0700

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -10,14 +10,4 @@ export PATH=$PATH:/opt/spark/bin:/opt/spark/sbin
 export PYSPARK_PYTHON=/usr/bin/python3
 EOF
 source ~/.bashrc
-mv /opt/spark/conf/spark-env.sh.template spark-env.sh
-sudo cat >> /opt/spark/conf/spark-env.sh <<EOF
-export SPARK_MASTER_HOST=<MASTER-IP>
-export JAVA_HOME=</usr/lib/jvm/java-1.8.0-openjdk-amd64>
-EOF
-mv slaves.template slaves
-sudo cat >> /opt/spark/conf/slaves <<EOF
-slave01
-slave02
-EOF
 
