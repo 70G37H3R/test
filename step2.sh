94324e71806e2b2f291a03ca0e73ae8c6bbe1288
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 02:59:19 +0700

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,10 +1,10 @@
-mv spark-env.sh.template spark-env.sh
-cat >> spark-env.sh <<EOF
+mv /opt/spark/conf/spark-env.sh.template /opt/spark/conf/spark-env.sh
+cat >> /opt/spark/conf/spark-env.sh <<EOF
 export SPARK_MASTER_HOST=192.168.100.0
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 EOF
-mv slaves.template slaves
-cat >> slaves <<EOF
+mv /opt/spark/conf/slaves.template /opt/spark/conf/slaves
+cat >> /opt/spark/conf/slaves <<EOF
 slave01
 slave02
 EOF
