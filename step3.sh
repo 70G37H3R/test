872c76a222e476c6ea653f76707fe3369799a14d
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 03:27:55 +0700

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -4,6 +4,7 @@ sudo pip3 install elephas
 sudo pip3 install tensorflow==1.15.0
 sudo pip3 install keras==2.2.4
 sudo pip3 install flask==1.1.2
+sudo pip3 install sklearn
 sudo pip3 install gevent
 sudo pip3 install opencv-python
 start-all.sh 
