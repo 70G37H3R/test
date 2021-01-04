c6b50cc17b31387b90cb417e267906cf8c2e6a56
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 03:29:30 +0700

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -8,4 +8,4 @@ sudo pip3 install sklearn
 sudo pip3 install gevent
 sudo pip3 install opencv-python
 start-all.sh 
-spark-submit  --master spark://master:7077 --executor-memory 4g --driver-memory 4g --total-executor-cores 4 --conf “spark.rpc.message.Maxsize=512” resnet_3_node.py 
+spark-submit  --master spark://master:7077 --executor-memory 4g --driver-memory 10g --total-executor-cores 4 --conf “spark.rpc.message.Maxsize=512” resnet_3_node.py 
