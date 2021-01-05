2ca146ec55362e60f9dac2f04e830eaaea2b6494
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 23:40:09 +0700

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -9,6 +9,7 @@ export SPARK_HOME=/opt/spark
 export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
 export PYSPARK_PYTHON=/usr/bin/python3
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=\$PATH:\$JAVA_HOME/bin
 EOF
 source ~/.bashrc
 
