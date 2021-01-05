0bce2d884c8319e158900c71d216cc64be2f3a89
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 23:29:42 +0700

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,6 +1,6 @@
 ssh-keygen
-cat   .ssh/id_rsa.pub  > .ssh/authorized_keys
-echo 'StrictHostKeyChecking no' >>  .ssh/config
+cat ~/.ssh/id_rsa.pub  > ~/.ssh/authorized_keys
+echo 'StrictHostKeyChecking no' >>  ~/.ssh/config
 
-#scp -r .ssh/  master:~/
-#scp -r .ssh/  slave01:~/
+#scp -r ~/.ssh/  master:~/
+#scp -r ~/.ssh/  slave01:~/
