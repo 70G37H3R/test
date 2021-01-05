f5f583c4092b804cf6cf807b1a52bde17d0a7155
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 23:51:26 +0700

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,3 +1,9 @@
+sudo apt update -y 
+sudo apt install openjdk-8-jdk scala -y
+wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+tar xvf spark-3.0.1-bin-hadoop3.2.tgz
+sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark
+
 sudo cat >> ~/.bashrc <<EOF
 export SPARK_HOME=/opt/spark
 export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin
