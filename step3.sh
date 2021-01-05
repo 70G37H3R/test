de0e96304bf8b4f618a26aed73727d4845932873
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-05 23:18:32 +0700

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -1,7 +1,7 @@
 mv /opt/spark/conf/spark-env.sh.template /opt/spark/conf/spark-env.sh
 cat >> /opt/spark/conf/spark-env.sh <<EOF
 export SPARK_MASTER_HOST='192.168.100.0'
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 EOF
 mv /opt/spark/conf/slaves.template /opt/spark/conf/slaves
 cat >> /opt/spark/conf/slaves <<EOF
