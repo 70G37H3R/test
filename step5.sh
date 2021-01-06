6eb2207e028516e6c1c02178779153f70fe7a6ed
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-06 13:50:09 +0700

    Update step5.sh

--- step5.sh
+++ step5.sh
@@ -1,12 +1,4 @@
-wget http://mirrors.viethosting.com/apache/hadoop/common/hadoop-2.9.2/hadoop-2.9.2.tar.gz
-tar xzf  hadoop-2.9.2.tar.gz 
-mv hadoop-2.9.2 ~/hadoop
-
-sudo cat >> ~/.bashrc <<EOF
-#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-#export PATH=\$PATH:\$JAVA_HOME/bin:
-export HADOOP_HOME=~/hadoop
-export PATH=\$PATH:\$HADOOP_HOME/sbin:\$HADOOP_HOME/bin 
-export HADOOP_CLASSPATH=\$JAVA_HOME/lib/tools.jar
-EOF
-source ~/.bashrc
+Cai hadoop
+spark sbumit server.py
+hdfs dfs -put <tenfile> /
+nano engine.py
