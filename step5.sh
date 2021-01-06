6785794fec3743ab0fa78079b5c2bcd1d73412e2
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-01-06 13:58:19 +0700

    Update step5.sh

--- step5.sh
+++ step5.sh
@@ -2,3 +2,7 @@ Cai hadoop
 spark sbumit server.py
 hdfs dfs -put <tenfile> /
 nano engine.py
+sudo pip install cherrypy
+sudo pip3 install pandas
+sudo pip3 install  flask_cors
+sudo pip3 install paste
