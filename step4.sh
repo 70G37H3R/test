ba88680c4381ee309ae84f8c312b74cd31e94537
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-03-11 21:24:45 +0700

    Update step4.sh

--- step4.sh
+++ step4.sh
@@ -10,5 +10,4 @@ sudo pip3 install opencv-python
 #stop-all.sh
 #start-all.sh
 #Tat proxy
-#spark-submit  --master spark://master:7077 --executor-memory 4g --driver-memory 10g 
---total-executor-cores 4 --conf "spark.rpc.message.maxSize=512" resnet_3_node.py 
+#spark-submit  --master spark://master:7077 --executor-memory 4g --driver-memory 10g --total-executor-cores 4 --conf "spark.rpc.message.maxSize=512" resnet_3_node.py 
